0352047d5ddf1cb664d037c10467801aec5c247a
古春雄 <chunxiong-gu@msyc.cc>
2021-05-13 16:56:58 +0800

    delete unUse codes

--- jetpackmvvm/src/main/java/com/example/jetpackmvvm/base/activity/BaseVmActivity.kt
+++ jetpackmvvm/src/main/java/com/example/jetpackmvvm/base/activity/BaseVmActivity.kt
@@ -1,7 +1,5 @@
 package com.example.jetpackmvvm.base.activity
 
-import android.app.Activity
-import android.app.AppDetailsActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
